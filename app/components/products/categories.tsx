a62f37524a822c48e2781c117b5cc7d64ba2e933
EDWARD MAKOPA <makopaedward@gmail.com>
2026-06-27 22:56:59 +0200

    ndaikama picture and ndakomza css uja amavuta kulandila background image

--- app/components/products/categories.tsx
+++ app/components/products/categories.tsx
@@ -6,14 +6,14 @@ export default function Categories() {
         <div>
             <h4 className="color-red-600">Featured Categories</h4>
             <div className="categories">
-                 <Product name="Fashion" image="../Fashion.png" />
-                 <Product name="Electronics" image="../Electronics.png" />
-                 <Product name="Home & Kitchen" image="../Home&Kitchen.png" />
-                 <Product name="Beauty" image="../Beauty.png" />
-                 <Product name="Sports" image="../Sports.png" />
-                 <Product name="Toys & Games" image="../Toys&Games.png" />
-                 <Product name="Books" image="../Books.png" />
-                 <Product name="Automotive" image="../Automotive.png" />
+                 <Product name="Fashion" image="../images/shirt.png" />
+                 <Product name="Electronics" image="../images/Ram.png" />
+                 <Product name="Home & Kitchen" image="../images/pot.jpeg" />
+                 <Product name="Beauty" image="../images/oil2.jpg" />
+                 <Product name="Sports" image="../images/ball.jpeg" />
+                 <Product name="Toys & Games" image="../images/toys.jpeg" />
+                 <Product name="Books" image="../images/books.jpeg" />
+                 <Product name="Shoes" image="../images/shoes.jpeg" />
             </div>
         </div>
     )
